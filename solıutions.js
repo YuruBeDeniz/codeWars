3b0a80294faa180865cf03fc7eabc723e7544b13
Deniz Saganak <saganak.deniz@gmail.com>
2022-08-17 20:58:40 +0200

    add kata

--- solıutions.js
+++ solıutions.js
@@ -68,3 +68,34 @@ https://www.codewars.com/kata/55c45be3b2079eccff00010f/train/javascript */
   function order(words){
     return words.split(' ').sort((a,b) => a.match(/\d/) - b.match(/\d/)).join(' ')
   }
+
+/* You get an array of arrays.
+If you sort the arrays by their length, you will see, that their length-values are consecutive.
+But one array is missing!
+
+
+You have to write a method, that return the length of the missing array.
+
+Example:
+[[1, 2], [4, 5, 1, 1], [1], [5, 6, 7, 8, 9]] --> 3
+
+If the array of arrays is null/nil or empty, the method should return 0.
+
+When an array in the array is null or empty, the method should return 0 too!
+There will always be a missing element and its length will be always between the given arrays.
+https://www.codewars.com/kata/57b6f5aadb5b3d0ae3000611/train/javascript */
+
+  function getLengthOfMissingArray(arr) {
+    let sortedByLength = (arr || []).map(a => a ? a.length : 0).sort((a,b) => a-b);
+    
+      if(sortedByLength.includes(0)){
+        return 0
+      }
+    
+    for(let i = 0; i < sortedByLength.length -1; i++){
+      if(sortedByLength[i] + 1 !== sortedByLength[i+1]){
+        return sortedByLength[i] + 1
+      }
+    }
+      return 0
+    }
