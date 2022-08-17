defc15037baba103c8dbfc6f65f884be48d49e36
Deniz Saganak <saganak.deniz@gmail.com>
2022-08-17 11:32:49 +0200

    add kata

--- solıutions.js
+++ solıutions.js
@@ -38,8 +38,33 @@ Notes
 All numbers are valid Int32, no need to validate them.
 There will always be at least one number in the input string.
 Output string must be two numbers separated by a single space, and highest number is first. */
- 
+
  function highAndLow(numbers){
     const sortedArray = numbers.split(' ').sort((a,b) => b-a)
     return sortedArray[0] + ' ' + sortedArray[sortedArray.length-1]
+  }
+
+  function highAndLow(numbers){
+    let arr = numbers.split(' ').map(Number);  
+    return Math.max(...arr) + ' ' + Math.min(...arr);
+  }
+
+  function highAndLow(numbers){
+    numbers = numbers.split(' ');
+    return `${Math.max(...numbers)} ${Math.min(...numbers)}`;
+  }
+
+/* Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+
+Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+
+Examples
+"is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+"4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+""  -->  ""
+https://www.codewars.com/kata/55c45be3b2079eccff00010f/train/javascript */
+  function order(words){
+    return words.split(' ').sort((a,b) => a.match(/\d/) - b.match(/\d/)).join(' ')
   }
