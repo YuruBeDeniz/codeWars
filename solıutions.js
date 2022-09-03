954212b5e5b43036b134559c46735e9a83d32eaa
Deniz Saganak <saganak.deniz@gmail.com>
2022-09-03 12:11:57 +0200

    add kata 5 kyu

--- solıutions.js
+++ solıutions.js
@@ -254,3 +254,47 @@ function plusMinus(arr) {
 }
 
 plusMinus([-4, 3, -9, 0, 4, 1]) // 0.500000  0.333333 0.166667
+
+function miniMaxSum(arr) {
+  const ascending = arr.sort((a,b) => a-b)
+  let minSum = 0;
+let maxSum = 0;
+  for(let i = 0; i < ascending.length; i++) {
+    minSum += ascending[i]
+    maxSum += ascending[i]
+    console.log()
+  }
+  minSum = minSum - ascending[4]
+  maxSum = maxSum - ascending[0]
+console.log(minSum,   maxSum)	
+}
+
+miniMaxSum([1,3,5,7,9]) //16 24
+
+
+/* The marketing team is spending way too much time typing in hashtags.
+Let's help them with our own Hashtag Generator!
+
+Here's the deal:
+
+It must start with a hashtag (#).
+All words must have their first letter capitalized.
+If the final result is longer than 140 chars it must return false.
+If the input or the result is an empty string it must return false.
+https://www.codewars.com/kata/52449b062fb80683ec000024/train/javascript 
+5kyu */
+
+function generateHashtag (str) {
+  let result = '#' + str.split(' ').map(e => e.charAt(0).toUpperCase()  + e.slice(1)).join('')
+  if (str.length === 0 || result.length > 140 || result.length === 0 || result === '#'){
+    return false
+   }
+  return result
+}
+
+generateHashtag("Do We have A Hashtag") // "#DoWeHaveAHashtag"
+generateHashtag("Looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong Cat") // false
+generateHashtag("Codewars") // "#Codewars"
+generateHashtag("code" + " ".repeat(140) + "wars") //"#CodeWars"
+generateHashtag("")
+generateHashtag(" ".repeat(200)) //false
