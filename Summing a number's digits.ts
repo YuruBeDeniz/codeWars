e5412a974b94749a8a1e045fafccd47f2e4b3b6a
Deniz Saganak <saganak.deniz@gmail.com>
2022-11-25 13:47:03 +0100

    add kata

---
+++ Summing a number's digits.ts
@@ -0,0 +1,18 @@
+/* Write a function named sumDigits which takes a number as input and returns the sum of the absolute value of each of the number's decimal digits.
+
+For example: (Input --> Output)
+
+10 --> 1
+99 --> 18
+-32 --> 5
+Let's assume that all numbers in the input will be integer values.
+7kyu
+https://www.codewars.com/kata/52f3149496de55aded000410/typescript */
+
+export function parse(data: string): number[] {
+    return String(Math.abs(n)).split('').reduce((accumulator, value) => Number(accumulator) + Number(value), 0);
+}
+  
+  console.log(sumDigits(10))//, 1);
+  console.log(sumDigits(99))//, 18);
+  console.log(sumDigits(-32))//, 5
--- sum of angles.ts
+++ sum of angles.ts
@@ -6,3 +6,4 @@ https://www.codewars.com/kata/5a03b3f6a1c9040084001765/typescript
 export function angle(n: number): number {
     return (n-2) * 180
   }
+  
