408db01bef0e2c0e4d58f53276f8f6604e1bb968
Deniz Saganak <saganak.deniz@gmail.com>
2022-12-08 17:20:01 +0100

    add kata

---
+++ Sum of Digits/Multiplication table.ts
@@ -0,0 +1,26 @@
+/* Your task, is to create NxN multiplication table, of size provided in parameter.
+
+for example, when given size is 3:
+
+1 2 3
+2 4 6
+3 6 9
+for given example, the return value should be: [[1,2,3],[2,4,6],[3,6,9]]
+6kyu
+https://www.codewars.com/kata/534d2f5b5371ecf8d2000a08/train/typescript */
+
+export function multiplicationTable (size: number): number[][] {
+    let result = [];
+    for(let i = 1; i < size+1; i++){
+      let subArray = [];
+      for(let j = 1; j <size+1; j++){
+        subArray.push(i*j)
+      }
+      result.push(subArray)
+    }
+    return result;      
+  }
+  
+  multiplicationTable(1)//, [[1]])
+  multiplicationTable(2)//, [[1, 2], [2, 4]])
+  multiplicationTable(3)//, [[1, 2, 3], [2, 4, 6], [3, 6, 9]]
--- Sum of Digits/Regex Password Validation.js
+++ Sum of Digits/Regex Password Validation.js
@@ -12,3 +12,61 @@ https://www.codewars.com/kata/52e1476c8147a7547a000811/javascript
 5kyu*/
 
 const REGEXP = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])[A-Za-z0-9]{6,}$/;
+
+
+class Character {
+    constructor(name, weapon) {
+        this.name = name;
+        this.weapon = weapon;
+    }
+
+attack() {
+    return 'attack with ' + this.weapon;
+}   
+}
+
+
+class Elf extends Character {
+    constructor(name, weapon, type){
+        super(name, weapon);
+        console.log(this);
+        this.type = type;
+    }
+}
+
+class Ogre extends Character {
+    constructor(name, weapon, color){
+        super(name, weapon);
+        this.color = color;
+    }
+    makeFort(){
+        return "strongest fort in the world made";
+    }
+}
+
+const shrek = new Ogre('Shrek', 'club', 'green');
+shrek.attack();
+shrek.makeFort();
+
+const dolby = new Elf('Dolby' , 'cloth', 'house');
+dolby.attack();
+
+/* console:
+Elf {
+  name: 'Dolby',
+  weapon: 'cloth',
+  __proto__: { constructor: ƒ Elf() }
+}
+
+'attack with cloth' */
+
+const fiona = new Elf('Fiona', 'ninja stars');
+console.log(fiona)
+/* console:
+
+Elf {
+  name: 'Fiona',
+  weapon: 'ninja stars',
+  __proto__: { constructor: ƒ Elf() }
+
+} */
